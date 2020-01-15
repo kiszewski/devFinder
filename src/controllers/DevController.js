c0a8ec766f3b9a6ad801de170ba145b76c6beb14
kiszewski <53118132+kiszewski@users.noreply.github.com>
2020-01-14 22:08:35 -0300

    Construído backend

---
+++ src/controllers/DevController.js
@@ -0,0 +1,42 @@
+const axios = require('axios')
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+    async store(req, res) {
+        const { github_username, techs, latitude, longitude } = req.body
+
+        let dev = await Dev.findOne({ github_username })
+
+        if (!dev) {
+            //chamando api do github
+            const response = await axios.get(`https://api.github.com/users/${github_username}`)
+
+            const { name = login, avatar_url, bio } = response.data
+
+            const techsArray = parseStringAsArray(techs)
+
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude]
+            }
+
+            dev = await Dev.create({
+                github_username,
+                name,
+                techs: techsArray,
+                avatar_url,
+                bio,
+                location
+            })
+
+        }
+        return res.json(dev)
+    },
+
+    async index(req, res) {
+        const devs = await Dev.find()
+
+        return res.json(devs)
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+
+mongoose.connect('mongodb+srv://kiszewski:kiszewski@cluster0-mxtge.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(3333)
